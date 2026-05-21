17631ef7771b1945aab561b6fa429620a16e04e7
Developer <you@example.com>
2026-05-21 07:44:07 +0000

    Add models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=255)
+
+    slug = models.SlugField(unique=True)
+
+    summary = models.TextField()
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
